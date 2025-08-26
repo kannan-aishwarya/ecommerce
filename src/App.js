95409c5c629fffcd3e8f4ddea381683740d7aa25
Aishwarya <aishoi232002@gmail.com>
2025-08-26 21:38:08 +0530

    Login functionality implemented

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Signup from './pages/signup';
+import Login from './pages/login';
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={<Login />} />
     </Routes>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -3,37 +3,37 @@ import ReactDOM from "react-dom/client";
 import {auth}  from "../firebase";
 
 import "./signup.css";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router-dom";
 
-function Signup() {
+function Login() {
 
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
 
     function handleSubmit(e) {
         e.preventDefault();
-        createUserWithEmailAndPassword(auth, email, password)
+        signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-        console.log("User registered:", userCredential.user);
-        })
-        .catch((error) => {
-          console.error("Error:", error.message);
-        });
+        console.log("User logged in:", userCredential.user);
+      })
+      .catch((error) => {
+       console.error("Error:", error.message);
+      });
     }
 
     return (
         <div className="signup">
             <div className="container">
-                <h1 className="header">Sign Up</h1>
+                <h1 className="header">Log In</h1>
                 <form className="form">
                     <input type="email" placeholder="Email" className="input-field" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <br />
                     <input type="password" placeholder="Password" className="input-field" value={password} onChange={(p) => setPassword(p.target.value)} />
                     <br/>
-                    <button type="submit" className="submit-button" onClick={handleSubmit}>Register</button>
-                     <br />
-                    <Link to="/">Return to Login</Link>
+                    <button type="submit" className="submit-button" onClick={handleSubmit}>Login</button>
+                    <br />
+                    <Link to="/signup">Don't have an account? Sign Up</Link>
                 </form>
             </div>
         </div>
@@ -42,4 +42,4 @@ function Signup() {
 }
 
 
-export default Signup;
+export default Login;
